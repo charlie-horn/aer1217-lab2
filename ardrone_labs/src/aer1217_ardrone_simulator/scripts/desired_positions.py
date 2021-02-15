62becdd7404afd9b538a743a62e58c037dc7d03e
deminy717 <ny.demi.717@gmail.com>
2021-02-15 07:06:56 -0500

    add comment

--- ardrone_labs/src/aer1217_ardrone_simulator/scripts/desired_positions.py
+++ ardrone_labs/src/aer1217_ardrone_simulator/scripts/desired_positions.py
@@ -19,31 +19,39 @@ class ROSDesiredPositionGenerator(object):
     """ROS interface for publishing desired positions."""
     # write code here for desired position trajectory generator
     def __init__(self):
+        # subscriber & publisher
         self.vicon_topic = '/vicon/ARDroneCarre/ARDroneCarre'
         self.sub_vicon = rospy.Subscriber(self.vicon_topic, TransformStamped, self.get_vicon_data)
         self.pub_traj = rospy.Publisher('/desired_position', Twist, queue_size=10)
-        self.path_des = 'lin' #'cir'
 
-        self.freq = 3 #10 #50
-
+        # initialize parameters
         self.x_des = []
         self.y_des = []
         self.z_des = []
         self.yaw_des = []
-        self.count = 0
 
         self.x = 0
         self.y = 0
         self.z = 0
         self.yaw = 0
 
-        self.total_count = 50 # 50 points for linear, 100 points for circular
+        self.count = 0
         self.thresh = 0.1
-        self.linear(self.total_count/2)
-        #self.circular(self.total_count / 2)
-        self.traj_timer = rospy.Timer(rospy.Duration(1. / self.freq), self.pub_des_pos)
+
+        self.freq = 3
+        
+        # select trajectory
+        self.total_count = 2  # num of waypoints: 2 points for linear, 25 points for circular
+        self.linear(self.total_count / 2)
+        # self.total_count = 25  # num of waypoints: 2 points for linear, 25 points for circular
+        # self.circular(self.total_count / 2)
+
+        self.path_des = 'lin'  # 'cir'  # parameter for circular v2
+	
+	self.traj_timer = rospy.Timer(rospy.Duration(1. / self.freq), self.pub_des_pos)
 
     def linear(self, n):
+        # linear trajectory: n - [int] - 0.5 * num of waypoints
         x1 = np.linspace(-1, 1, n)
         x2 = np.linspace(1, -1, n)
         self.x_des = np.concatenate((x1, x2))
@@ -56,11 +64,13 @@ class ROSDesiredPositionGenerator(object):
         z2 = np.linspace(2, 1, n)
         self.z_des = np.concatenate((z1, z2))
 
-        self.yaw_des = np.zeros(2*n) #?
+        self.yaw_des = np.zeros(2*n)
 
     def circular(self, n):
+        # circular trajectory: n - [int] - 0.5 * num of waypoints
         r = 1
         theta = np.linspace(0, 2*np.pi, n*2)
+
         self.x_des = np.cos(theta) * r
         self.y_des = np.sin(theta) * r
 
@@ -68,12 +78,12 @@ class ROSDesiredPositionGenerator(object):
         z2 = np.linspace(1.5, 0.5, n)
         self.z_des = np.concatenate((z1, z2))
 
-        #self.yaw_des = np.zeros(2 * n)
-
         self.yaw_des = np.linspace(-np.pi, np.pi, n*2)
 
-    # callback function for vicon
+
     def get_vicon_data(self, vicon_msg):
+        # callback function for vicon
+        # get actual current position and yaw
         self.x = vicon_msg.transform.translation.x
         self.y = vicon_msg.transform.translation.y
         self.z = vicon_msg.transform.translation.z
@@ -85,6 +95,7 @@ class ROSDesiredPositionGenerator(object):
         self.yaw = euler[2]
 
     def pub_des_pos(self, i):
+        # publish desired position
         msg = Twist()
         msg.linear.x = self.x_des[self.count]
         msg.linear.y = self.y_des[self.count]
@@ -92,50 +103,53 @@ class ROSDesiredPositionGenerator(object):
         msg.angular.z = self.yaw_des[self.count]
 
         self.pub_traj.publish(msg)
-        
-        if self.path_des == 'cir' or self.path_des == 'lin':
-            if  self.count == 0 or self.count == floor(self.total_count/2)-1:
-                update_count = self.check_dist(self.x_des[self.count], self.x) and \
-                               self.check_dist(self.y_des[self.count], self.y) and \
-                               self.check_dist(self.z_des[self.count], self.z) and \
-                               self.yaw_check(self.yaw_des[self.count], self.yaw)
-            else:
-                update_count = 1
-        else:
-            update_count = self.check_dist(self.x_des[self.count], self.x) and \
-                           self.check_dist(self.y_des[self.count], self.y) and \
-                           self.check_dist(self.z_des[self.count], self.z) and \
-                           self.yaw_check(self.yaw_des[self.count], self.yaw)
 
-#         update_count = self.check_dist(self.x_des[self.count], self.x) and \
-#                        self.check_dist(self.y_des[self.count], self.y) and \
-#                        self.check_dist(self.z_des[self.count], self.z) and \
-#                        self.yaw_check(self.yaw_des[self.count], self.yaw)
+        # update count if the difference between the actual position and the desired position is less than the threshold
+        update_count = self.check_dist(self.x_des[self.count], self.x) and \
+                       self.check_dist(self.y_des[self.count], self.y) and \
+                       self.check_dist(self.z_des[self.count], self.z) and \
+                       self.yaw_check(self.yaw_des[self.count], self.yaw)
+
+        ############################ circular v2 - only 2 checkpoints ########################
+#         if self.path_des == 'cir' or self.path_des == 'lin':
+#             if  self.count == 0 or self.count == floor(self.total_count/2)-1:
+#                 update_count = self.check_dist(self.x_des[self.count], self.x) and \
+#                                self.check_dist(self.y_des[self.count], self.y) and \
+#                                self.check_dist(self.z_des[self.count], self.z) and \
+#                                self.yaw_check(self.yaw_des[self.count], self.yaw)
+#             else:
+#                 update_count = 1
+#         else:
+#             update_count = self.check_dist(self.x_des[self.count], self.x) and \
+#                            self.check_dist(self.y_des[self.count], self.y) and \
+#                            self.check_dist(self.z_des[self.count], self.z) and \
+#                            self.yaw_check(self.yaw_des[self.count], self.yaw)
+        #######################################################################################
 
         if update_count:
             if self.count < self.total_count-1:
                 self.count += 1
-                print('x = '.format(self.x_des[self.count]))
-                print('y = '.format(self.y_des[self.count]))
-                print('z = '.format(self.z_des[self.count]))
             else:
                 self.count = 0
 
-    # check distance of desired and actual value to determine if proceed
+
     def check_dist(self, des, act):
+        # check difference between desired and actual position to determine if proceed
         if abs(des - act) > self.thresh:
             return False
         else:
             return True
 
     def yaw_check(self,des,act):
+        # check difference between desired and actual yaw to determine if proceed
         yaw_error = des - act
-        if yaw_error > np.pi:
+        if yaw_error > np.pi: # wrap to pi
             yaw_error = yaw_error - 2 * np.pi
         if yaw_error > self.thresh:
             return False
         else:
             return True
+
 
 if __name__ == '__main__':
     rospy.init_node('desired_positions')
